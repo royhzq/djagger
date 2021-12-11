14f81cf25acbb2ef7c183617b3ebee0c76046434
Ubuntu <ubuntu@ip-172-31-4-236.ap-southeast-1.compute.internal>
2021-12-11 12:54:39 +0000

    Fixed template bugs

--- djagger/urls.py
+++ djagger/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import redoc, open_api_json
 
 urlpatterns = [
-    path('api/docs', redoc, name='api_doc'),
-    path('api/open-api',open_api_json, name='open_api_json')
+    path('api/docs', redoc, name='djagger_doc'),
+    path('api/schema.json',open_api_json, name='djagger_json')
 ]
  
