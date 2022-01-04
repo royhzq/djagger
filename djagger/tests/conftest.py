79a89530bf0da91d2fd8300f29de90f3c85c4800
Ubuntu <ubuntu@ip-172-31-4-236.ap-southeast-1.compute.internal>
2022-01-04 07:54:41 +0000

    Updated tests

--- djagger/tests/conftest.py
+++ djagger/tests/conftest.py
@@ -1,11 +1,13 @@
-import pytest
 from django.conf import settings
+from typing import List
+import pytest
 import django
+
+urlpatterns: List = []
+
 
 @pytest.fixture(scope="session", autouse=True)
 def setup(request):
     # prepare django ahead of all tests
-    settings.configure()
+    settings.configure(ROOT_URLCONF=__name__)
     django.setup()
-
-
--- djagger/tests/tests.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.test import TestCase
-
-from ..openapi import Document
-
-
-def test_document():
-    Document.generate()
